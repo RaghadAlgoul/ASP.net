a02cadab525fdd91dde4ef433e5baeb5a2040601
Raghad Alghoul <102190199+RaghadAlgoul@users.noreply.github.com>
2023-01-02 23:20:43 +0200

    t1

---
+++ WebUserControl1.ascx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1_2
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                HttpCookie cookies = Request.Cookies["opinion"];
+
+                if (cookies != null)
+                {
+                    
+                    SqlConnection connection = new SqlConnection("Data Source=DESKTOP-LPCCUNQ\\SQLEXPRESS;Database=tasks;Integrated Security=True;");
+                    SqlCommand command1 = new SqlCommand("select sum(exe)from vote;", connection);
+                    try
+                    {
+                        connection.Open();
+
+                        Label5.Text =  command1.ExecuteScalar().ToString();
+                        connection.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Can not open connection ! ");
+                    }
+                    SqlCommand command2 = new SqlCommand("select sum(vg)from vote;", connection);
+                    try
+                    {
+                        connection.Open();
+
+                        Label6.Text = command1.ExecuteScalar().ToString();
+                        connection.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Can not open connection ! ");
+                    }
+                    SqlCommand command3 = new SqlCommand("select sum(g)from vote;", connection);
+                    try
+                    {
+                        connection.Open();
+
+                        Label7.Text = command1.ExecuteScalar().ToString();
+                        connection.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Can not open connection ! ");
+                    }
+                    
+                    SqlCommand command5 = new SqlCommand("select sum(p)from vote;", connection);
+                    try
+                    {
+                        connection.Open();
+
+                        Label8.Text = command1.ExecuteScalar().ToString();
+                        connection.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Can not open connection ! ");
+                    }
+                }
+
+
+
+            }
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            HttpCookie newCooki = Request.Cookies["opinion"];
+            if (newCooki != null)
+            {
+                newCooki.Values["opinion"] = "vote";
+                Label1.Text = newCooki["opinion"];
+
+
+            }
+            else
+            {
+                newCooki = new HttpCookie("opinion");
+                newCooki.Values["opinion"] = "votet";
+                Label1.Text = newCooki["opinion"];
+                using (
+               SqlConnection connection = new SqlConnection("Data Source=DESKTOP-LPCCUNQ\\SQLEXPRESS;Database=tasks;Integrated Security=True;")
+               )
+                {
+                    SqlCommand comm = new SqlCommand($"insert into vote values (@r1,@r2,@r3,@r4)", connection);
+                    if (RadioButton1.Checked) { comm.Parameters.AddWithValue("@r1", 1); } else { comm.Parameters.AddWithValue("@r1", 0); }
+                    if (RadioButton2.Checked) { comm.Parameters.AddWithValue("@r2", 1); } else { comm.Parameters.AddWithValue("@r2", 0); }
+                    if (RadioButton3.Checked) { comm.Parameters.AddWithValue("@r3", 1); } else { comm.Parameters.AddWithValue("@r3", 0); }
+                    if (RadioButton4.Checked) { comm.Parameters.AddWithValue("@r4", 1); } else { comm.Parameters.AddWithValue("@r4", 0); }
+
+
+                    comm.Connection.Open();
+                    comm.ExecuteNonQuery();
+                }
+
+
+
+            }
+            newCooki.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(newCooki);
+           
+
+        }
+
+
+    }
+}
